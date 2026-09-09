21041c1c20984a2cbf496d5dcbfb3bd8a7ab7b57
NicholasPimenta <nicholas2ilva@gmail.com>
2026-09-08 22:05:44 -0300

    feat: add responsive weather overlay

--- src/components/WeatherResult/WeatherResult.tsx
+++ src/components/WeatherResult/WeatherResult.tsx
@@ -1,7 +1,7 @@
 import { ArrowLeftIcon } from "@phosphor-icons/react";
 import styles from "./WeatherResult.module.css";
 import clearDayVideo from "../../assets/weather/clear-day.mp4";
-import clearDayFallback from "../../assets/weather/clear-sky.png";
+import clearDayFallback from "../../assets/weather/clear-day.png";
 
 interface ForecastDay {
   id: string;
